ff40266e296d52d3f7d363e0c54d50dd2d088bde
Lingxiao <klx798949379@gmail.com>
2023-02-26 14:11:25 +0100

    modify latent_pi_net

--- main.py
+++ main.py
@@ -1,22 +1,16 @@
 from robogym.utils.env_utils import load_env
-from ned2_wrapper import Ned2_Wrapper
-from stable_baselines3 import DDPG
-import matplotlib.pyplot as plt
+from stable_baselines3 import SAC
 from mod_sb3 import run
+import torch
 
 # train on sim env
 names = 'robogym_wrapper.py'
 kwargs = {'parameters': {'simulation_params': {'num_objects': 1}}}
 sim_env, args_remaining = load_env(names, return_args_remaining=True, **kwargs)
 run()
-model = DDPG("MultiInputPolicy", sim_env)
-model.learn(total_timesteps=100000, log_interval=10)
+model = SAC("MultiInputPolicy", sim_env)
+model.learn(total_timesteps=1000, log_interval=10)
 
-# deploy on real env
-real_env = Ned2_Wrapper("10.10.10.10", model, 100, 100)
-list_num_eval, list_reward = real_env.eval()
-
-# plot
-fig, ax = plt.subplots()
-ax.set(list_num_eval, list_reward)
-plt.show()
+torch.save(model.get_parameters(), 'model_robogym_torch.pth.tar', _use_new_zipfile_serialization=False)
+del model # remove to demonstrate saving and loading
+sim_env.close()
