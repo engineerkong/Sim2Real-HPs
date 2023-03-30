78a69199463c375ced40b8331c0065c76c0dae29
engineerkong <klx798949379@gmail.com>
2023-03-30 11:34:34 +0200

    change 1.4

---
+++ CHANGELOG.md
@@ -0,0 +1,8 @@
+# origin:
+https://github.com/NiryoRobotics/ned_ros.git
+
+# change 1 (31.March):
+ros_wrapper_mygym.py - a ros_wrapper for myygm
+deploy.py - set up a MygymEnv
+          - load trained policy and deploy the policy on ros
+
--- rosDeploy/src/niryo_robot_bringup/scripts/deploy.py
+++ rosDeploy/src/niryo_robot_bringup/scripts/deploy.py
@@ -9,21 +9,23 @@ from sb3_python2.model import Model
 from sb3_python2.policy import MlpPolicy, MultiInputPolicy
 from gym.spaces import Box, MultiDiscrete, Dict
 import rospy
+import numpy as np
 
 def recursive_getattr(obj, attr, *args):
     def _getattr(obj, attr):
         return getattr(obj, attr, *args)
     return functools.reduce(_getattr, [obj] + attr.split("."))
 
-class RobogymEnv:
+class MygymEnv:
 
     def __init__(self):
         
         self.observation_space = Box(-float('inf'), float('inf'), (13,))
         self.action_space = Box(-float(1), float(1), (6,))
-
+        self.action_space.high = np.array([2.99987191833, 0.610167106497, 1.57009819509, 2.09003177926, 1.92282923692, 2.53002928369])
+        self.action_space.low = np.array([-2.99987191833, -1.83259571459, -1.34006379968, -2.09003177926, -1.92003671012, -2.53002928369])
 print("---load---")
-env = RobogymEnv()
+env = MygymEnv()
 model = Model(env, MlpPolicy)
 params = torch.load('model_torch.pth.tar')
 for name in params:
